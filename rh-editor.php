594cbb8e070a17a766354dd5cf496078f79b8c45
Robin Herbeck <info@robinherbeck.com>
2026-06-17 12:55:26 +0200

    Core 2.4.1 bundeln: Loader-Fix fuer gemischte Versionen

--- rh-editor.php
+++ rh-editor.php
@@ -5,7 +5,7 @@
  * Plugin URI:        https://github.com/herbeckrobin/rh-editor
  * Update URI:        https://github.com/herbeckrobin/rh-editor
  * Description:       Editor-Souveränität: SVG-Upload mit Sanitisierung, Inserter aufräumen, genutzte Core-Blöcke in eine eigene Kategorie gruppieren, Block-Style-Helper. Teil der rh-blueprint Kollektion.
- * Version:           0.1.2
+ * Version:           0.1.3
  * Requires at least: 6.5
  * Requires PHP:      8.1
  * Author:            Robin Herbeck
@@ -21,7 +21,7 @@ if (! defined('ABSPATH')) {
     exit;
 }
 
-define('RHEDITOR_VERSION', '0.1.2');
+define('RHEDITOR_VERSION', '0.1.3');
 define('RHEDITOR_PLUGIN_FILE', __FILE__);
 define('RHEDITOR_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('RHEDITOR_PLUGIN_URL', plugin_dir_url(__FILE__));
